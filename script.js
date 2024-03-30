2a74ea6e7b59fc9f37775d094e66f21936a2ea3d
eddwardmessias <eddwardmessias@hotmail.com>
2024-03-30 11:10:34 -0300

    Enviando funcionalidade total e remove carrinho

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function updateCartModal(){
                 </div>
             
                 
-                <button>
+                <button class="remove-from-cart-btn" data-name="${item.name}">
                     Remover
                 </button>
             
@@ -97,4 +97,29 @@ function updateCartModal(){
 
     cartCounter.innerHTML = cart.length;
 
+}
+
+//Função para remover item do carrinho
+cartItemsContainer.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+        
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -=1;
+            updateCartModal()
+            return;
+        }
+        cart.splice(index,1);
+        updateCartModal();
+    }
 }
